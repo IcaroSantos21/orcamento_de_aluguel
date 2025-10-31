1568dc67473015984d8b10934b18ec53c54e1631
IcaroSantos21 <icaro.santosdev@gmail.com>
2025-10-31 13:02:02 -0300

    Finalizando o projeto

--- classe_orcamentos.py
+++ classe_orcamentos.py
@@ -1,18 +1,21 @@
+# Criação da classe de orçamentos
 class Orcamento():
     # Valor padrão por tipo de imóvel:
-    VALOR_APARTAMENTO = 700.00
-    VALOR_CASA = 900.00
-    VALOR_ESTUDIO = 1200.00
+    VALOR_APARTAMENTO = 700.00 # Valor base para apartamentos
+    VALOR_CASA = 900.00 # Valor base para casa
+    VALOR_ESTUDIO = 1200.00 # Valor base para estudio
     # Valor padrão do contrato
-    VALOR_CONTRATO = 2000.00
+    VALOR_CONTRATO = 2000.00 # Valor inteiro do contrato
     # Valor Padrão de adicionais
-    VALOR_QUARTO_CASA = 250.00
-    VALOR_QUARTO_APARTAMENTO = 200.00
-    VALOR_GARAGEM = 300.00
-    VALOR_ESTACIONAMENTO = 250.00
+    VALOR_QUARTO_CASA = 250.00 # Valor caso adicione mais um quarto para casa
+    VALOR_QUARTO_APARTAMENTO = 200.00 # Valor caso adicione mais um quarto para apartamento
+    VALOR_GARAGEM = 300.00 # Valor padrão para adicionar garagem
+    VALOR_ESTACIONAMENTO = 250.00 # Valor padrão para adicionar estacionamento de 2 vagas no estudio
+
 
     def __init__(self, tipo_imovel, numero_quartos=1, criancas=0, 
                  garagem=0, vagas_estacionamento=0, numero_parcela=1):
+        # Atributos da classe  de orçamento
         self.tipo_imovel = tipo_imovel
         self.quantidade_quartos = numero_quartos
         self.quantidade_criancas = criancas
@@ -21,34 +24,43 @@ class Orcamento():
         self.numero_parcelas = numero_parcela
 
     def calcular_valor_aluguel(self):
+        # Calcular o valor do imóvel com base no valor padrão e adicionais
         if self.tipo_imovel == 'apartamento':
             valor_base = self.VALOR_APARTAMENTO
             if self.quantidade_quartos > 1:
+                # Acréscimo do quarto
                 valor_base = self.VALOR_APARTAMENTO + (self.VALOR_QUARTO_APARTAMENTO * (self.quantidade_quartos - 1))
             if self.garagem >= 1:
+                # Acréscimo da garagem
                 valor_base += self.VALOR_GARAGEM
             if self.quantidade_criancas == 0:
+                # Desconto caso não tenha criança
                 valor_base *= 0.95
             return valor_base
         elif self.tipo_imovel == 'casa':
             valor_base = self.VALOR_CASA
             if self.quantidade_quartos > 1:
+                # Acréscimo do quarto
                 valor_base = self.VALOR_CASA + (self.VALOR_QUARTO_CASA * (self.quantidade_quartos - 1))
             if self.garagem >= 1:
+                # Acréscimo da garagem
                 valor_base += self.VALOR_GARAGEM
             return valor_base
         elif self.tipo_imovel == 'estudio':
             if self.vagas_estacionamento == 0:
                 valor_base = self.VALOR_ESTUDIO 
             elif self.vagas_estacionamento <= 2:
+                # Acréscimo do estacionamento até 2 vagas
                 valor_base = self.VALOR_ESTUDIO + self.VALOR_ESTACIONAMENTO
             else:
+                # Acréscimo do estacionamento acima de 2 vagas
                 valor_base = self.VALOR_ESTUDIO + self.VALOR_ESTACIONAMENTO + (60.00 * (self.vagas_estacionamento - 2))
-            return valor_base
+            return round(valor_base, 2)
         else:
             raise ValueError('Tipo de Imóvel inválido')
         
     def calcular_parcelas(self):
+        # Calculando as parcelas do contrato
         if self.numero_parcelas <= 0 or self.numero_parcelas > 5:
             raise ValueError('O número de parcelas deve ser entre 1 e 5 vezes ')
         parcela = round(self.VALOR_CONTRATO / self.numero_parcelas, 2)
@@ -56,6 +68,7 @@ class Orcamento():
 
     
     def __str__(self):
+        # Saída da classe orçamento
         if self.tipo_imovel == 'estudio':
             return f"""
             |-----------------------|
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from classe_orcamentos import Orcamento
 import csv
-lista_orcamentos = []
+lista_orcamentos = [] # Lista para armazenar os orçamentos
+
+# Função para criar o orçamento
 def criar_orçamento():  
     qtd_criancas = 0
     qtd_quartos = 0
@@ -14,17 +16,18 @@ def criar_orçamento():
                     '[Casa] | [Apartamento] | [Estudio]\n' \
                     'Digite Aqui: ').lower()
 
+    # Solicita a quantidade de vagas do estacionamento somente para o estudio
     if imovel == 'estudio':
         estacionamento = input('Gostaria de ter vagas de estacionamento? [S]Sim | [N]Não:').upper()
         if estacionamento == 'S':
             qtd_vagas = int(input('Quantidade de Vagas: '))
 
+    # Solicita dados para o orçamento de casa e/ou apartamento
     else:
         qtd_quartos = int(input('Digite a quantidade de quartos deseja: '))
         possui_crianca = input('Um dos moradores será uma criança? [S]Sim | [N]Não: ').upper()
         if possui_crianca == 'S':
             qtd_criancas = int(input('Digite a quantidade de crianças: '))
-            crianca = qtd_criancas
         possui_garagem = input('Deseja uma casa com garagem? [S]Sim | [N]Não:').upper()
         if possui_garagem == 'S':
             garagem = 1
@@ -32,6 +35,7 @@ def criar_orçamento():
     print(Orcamento.VALOR_CONTRATO)
     parcela_contrato = int(input('Deseja pagar em quantas vezes? em até 5x: '))
 
+    # cria o orçamento
     novo_orcamento = Orcamento(
         tipo_imovel = imovel,
         numero_quartos = qtd_quartos,
@@ -40,11 +44,14 @@ def criar_orçamento():
         vagas_estacionamento = qtd_vagas,
         numero_parcela = parcela_contrato
     )
-    lista_orcamentos.append(novo_orcamento)
-    return novo_orcamento
-def mostrar_orçamento(lista):
-    if not lista_orcamentos:
-        return 'Não existe nenhum orçamento na lista ainda'
+    lista_orcamentos.append(novo_orcamento) # adiciona o orçamento na lista
+    print(novo_orcamento)
+    return novo_orcamento # retorna o orçamento
+
+# função para mostrar a lista de orçamentos criados
+def mostrar_orcamento(lista):
+    if not lista:
+        print('Não existe nenhum orçamento na lista ainda')
         
     print(f'Existem um total de {len(lista_orcamentos)} orçamentos criados')
     for i, o in enumerate(lista_orcamentos, start=1):
@@ -55,11 +62,47 @@ def mostrar_orçamento(lista):
         print(lista_orcamentos[escolha - 1])
     else:
         print("Número inválido!")
-def criar_arquivo_csv():
-     ...
-# with open('orcamento.csv', 'w', newline='') as arquivo:
-#     escritor = csv.writer(arquivo)
-#     escritor.writerow(['MÊS ', ' VALOR'])
-#     for mes in range(1, 13):
-#         escritor.writerow([mes, orcamento.calcular_valor_aluguel()])
 
+# Função do menu de interação do usuário
+def mostrar_menu():
+    print('''
+       -----------------------------   
+        Selecione a Opção desejada:
+        [1] Fazer Orçamento
+        [2] Listar Orçamentos
+        [3] Sair do Programa
+       -----------------------------
+    ''')
+    return input('Digite o número aqui: ')
+
+# Função principal do sistema
+def main():
+    while True:
+        opcao = mostrar_menu()
+        if opcao == '1':
+            novo_orcamento = criar_orçamento()
+            with open('orcamento.csv', mode='a', newline='\n', encoding='utf-8-sig') as arquivo_csv:
+                escritor = csv.writer(arquivo_csv, delimiter=';')
+                escritor.writerow(['Orcamento'])
+                escritor.writerow([ 'Mês', 'Aluguel',])
+                for mes in range(1, 13):
+                    escritor.writerow([
+                        mes,
+                        novo_orcamento.calcular_valor_aluguel(),
+                        ])
+
+                        
+            print('Orçamento Gerado com sucesso')
+            input("\nPressione ENTER para voltar ao menu...")
+        elif opcao == '2':
+            mostrar_orcamento(lista_orcamentos)
+            input("\nPressione ENTER para voltar ao menu...")
+        elif opcao == '3':
+            print('Saindo...')
+            break
+        else:
+            print('Opção Inválida, por valor selecione uma válida!')
+            input("\nPressione ENTER para voltar ao menu...")
+
+if __name__ == '__main__':
+    main()
